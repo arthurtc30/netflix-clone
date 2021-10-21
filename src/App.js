f6b33bb868c751d47724e468654f3ec79ef5a7f7
Arthur Teixeira <52799942+arthurtc30@users.noreply.github.com>
2021-10-21 14:55:19 -0300

    Iniciando filme em destaque

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import './App.css';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
-
-  const [movieList, setMovieList] = useState([]);
+  const [movieList, setMovieList] = useState([]); // lista de filmes
+  const [featuredData, setFeaturedData] = useState(null); // filme em destaque
 
   useEffect(() => {
     const loadAll = async () => {
       // pegando a lista de todos os filmes
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      // pegando o filme em destaque
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      
+      // TODO continuar
+      // https://www.youtube.com/watch?v=tBweoUiMsDg
+      // 11:19:00
     }
 
     loadAll();
@@ -19,6 +28,11 @@ export default () => {
 
   return (
     <div className="page">
+
+      {featuredData && 
+        <FeaturedMovie item={featuredData} />
+      }
+
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
